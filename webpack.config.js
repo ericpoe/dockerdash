c69c1531c5c4a0278b5115bb8ba9ee778eff1bd4
Eric Poe <eric.poe@gmail.com>
2018-03-27 22:21:22 -0500

    Add vuejs

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,15 @@ Encore
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+    // empty the outputPath dir before each build
     .cleanupOutputBeforeBuild()
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
-
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
+    .enableVersioning(Encore.isProduction())
+    .enableVueLoader()
+    .addEntry('js/app', './assets/js/main.js')
     // .addStyleEntry('css/app', './assets/css/app.scss')
 
     // uncomment if you use Sass/SCSS files
