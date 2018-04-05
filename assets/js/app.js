1a843d980d0d3f616c78d35834cd4ab46dc874f2
Eric Poe <eric.poe@gmail.com>
2018-04-04 21:55:32 -0500

    Add axios for consuming APIs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import Dashboard from "./components/Dashboard";
+
+Vue.use(VueAxios, axios);
 
 new Vue({
   el: "#app",
