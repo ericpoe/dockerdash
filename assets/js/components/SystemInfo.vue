5e97a7f167d2e2b27b6bb960620a0dbfebf273ec
Eric Poe <eric.poe@gmail.com>
2018-04-05 12:46:33 -0500

    provide default values for systemInfo fields

--- assets/js/components/SystemInfo.vue
+++ assets/js/components/SystemInfo.vue
@@ -3,14 +3,14 @@
     <div class="px-4 py-2">
       <p class="text-grey-darker text-base">
         <span id="ServerVersion">
-          <strong class="font-bold">Server Version: </strong> {{ sysInfo.serverVersion }}
+          <strong class="font-bold">Server Version: </strong> {{ sysInfo.serverVersion || "Unknown" }}
         </span>
         <span id="Containers">
-          <strong class="font-bold">Containers: </strong> {{ sysInfo.containers }}
-          <strong class="font-bold">Containers Running: </strong> {{ sysInfo.containersrunning ? sysInfo.containersrunning : 0 }}
+          <strong class="font-bold">Containers: </strong> {{ sysInfo.containers || 0 }}
+          <strong class="font-bold">Containers Running: </strong> {{ sysInfo.containersrunning || 0 }}
         </span>
         <span id="Images">
-          <strong class="font-bold">Images: </strong> {{ sysInfo.images }}
+          <strong class="font-bold">Images: </strong> {{ sysInfo.images || 0 }}
         </span>
       </p>
     </div>
@@ -22,12 +22,6 @@ import axios from "axios";
 
 export default {
   name: "SystemInfo",
-  props: {
-    containersRunning: {
-      default: 0,
-      type: Number
-    }
-  },
   data() {
     return {
       sysInfo: {}
