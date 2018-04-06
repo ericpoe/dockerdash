faad5c7ea04b9483632fd21fd080841efb850e1c
Eric Poe <eric.poe@gmail.com>
2018-04-06 00:25:57 -0500

    annotation fix

---
+++ src/Controller/ContainerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Docker\Docker;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * @Route("/containers")
+ */
+class ContainerController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
+    public function index() : JsonResponse
+    {
+        $docker = Docker::create();
+
+        return $this->json($docker->containerList());
+    }
+}
